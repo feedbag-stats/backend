edf4f633e7e2a031c01863ec489cdc47f40e6015
Timothy Zemp <timothy@zemp.ch>
2019-03-12 14:07:19 +0100

    initial commit

---
+++ src/main/java/ch/uzh/feedbag/backend/controller/UserController.java
@@ -0,0 +1,29 @@
+package ch.uzh.feedbag.backend.controller;
+
+import ch.uzh.feedbag.shared.entity.user.User;
+import ch.uzh.feedbag.backend.repository.UserRepository;
+import ch.uzh.feedbag.backend.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService service;
+
+    UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    Iterable<User> all() {
+        return service.getUsers();
+    }
+
+    @PostMapping("/users")
+    User createUser(@RequestBody User newUser) {
+        return this.service.createUser(newUser);
+    }
+}
---
+++ src/main/java/ch/uzh/feedbag/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ch.uzh.feedbag.backend.repository;
+
+import ch.uzh.feedbag.shared.entity.user.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends CrudRepository<User, Long> {
+	User findByName(String name);
+	User findByUsername(String username);
+	User findByToken(String token);
+}
