a39da32ccdf1dc4163b98078b53a505059f35a4b
Sarah Zurmühle <s.zurmuehle@hispeed.ch>
2019-04-30 16:59:33 +0200

    Change commands in zipWatcher for new and removed files. Now all zip files are given as an output.

--- scripts/zipWatcher.sh
+++ scripts/zipWatcher.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-echo "new:"
+echo "processing..."
 
 #find added files
-diff -Ewbur $1 $2 -x '$1.zip' | grep $1 | grep -E "^Only in $1" | sed -n 's/://p' | awk '{print $3"/"$4}' 
+# diff -rq $1 $2 -x '$1.zip' | grep $1 | grep -E "^Only in $1" | sed -n 's/://p' | awk '{print $3"/"$4}' 
+# https://stackoverflow.com/questions/4997693/given-two-directory-trees-how-can-i-find-out-which-files-differ
+echo "new:"
+OUTPUT=$(rsync --recursive --delete --links --checksum --verbose --dry-run $1 $2)
+echo "${OUTPUT}" | grep -v '^deleting.*zip' | grep '.*zip'
 
+# #find removed files
+# diff -rq  $1 $2 -x '$2.zip' | grep $2 | grep -E "^Only in $2" | sed -n 's/://p' | awk '{print $3"/"$4}'
 echo "removed:"
-
-#find removed files
-diff -rq  $1 $2 -x '$2.zip' | grep $2 | grep -E "^Only in $2" | sed -n 's/://p' | awk '{print $3"/"$4}'
-
-echo "modified:"
+echo "${OUTPUT}" | grep '^deleting.*zip'
 
 # find modified files 
-diff -rq $1 $2 | grep $1 | grep -E "^Files $1" | awk '{print $2}' 
+echo "modified:"
+diff -rq $1 $2 -x '$2.zip' | grep $1 | grep -E "^Files $1" | awk '{print $2}'
