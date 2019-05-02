e44ae4166fffb92774336ff211436b83c10daa8f
Sarah Zurmühle <s.zurmuehle@hispeed.ch>
2019-05-02 15:01:58 +0200

    Change bash commands, so that the script runs faster. It is now solved with saving the find results of the folder in a temp txt file and then comparing these two files according to their lines.

--- scripts/zipWatcher.sh
+++ scripts/zipWatcher.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
 
-# https://stackoverflow.com/questions/4997693/given-two-directory-trees-how-can-i-find-out-which-files-differ
-
-echo "processing..."
-
-#find added files (modified files included)
-OUTPUT=$(rsync --recursive --delete --links --checksum --verbose --dry-run $1 $2)
 echo "\nnew:"
-echo "${OUTPUT}" | grep -v '^deleting.*zip' | grep '.*zip'
 
-# other alternative which does not always show all zip files
-# diff -rq $1 $2 -x '$1.zip' | grep $1 | grep -E "^Only in $1" | sed -n 's/://p' | awk '{print $3"/"$4}' 
+#find all files of first argument input and put it into a temp file
+find $1 | sort | cut -d "/" -f2 -f3 -f4 -f5 -f6 > temp.txt
 
-# #find removed files
+# find all added files in argument 1 compared to argument 2
+grep -v -F -x -f $2 temp.txt | grep ".*zip"
+
+# other alternatives which did not precicly work 
+# diff -rq $1 $2 -x '$1.zip' | grep $1 | grep -E "^Only in $1" | sed -n 's/://p' | awk '{print $3"/"$4}'
+# OUTPUT=$(rsync --recursive --delete --links --checksum --verbose --dry-run $1 $2)
+# echo "${OUTPUT}" | grep -v '^deleting.*zip' | grep '.*zip'
+#comm -2 temp.txt $2  
+
+
 echo "\nremoved:"
-echo "${OUTPUT}" | grep '^deleting.*zip' | grep ' .*zip' | awk '{print $2}'
 
-# other alternative which does not always show all zip files
+# find all removed files in argument 1 compared to argument 2
+grep -v -F -x -f temp.txt $2 | grep ".*zip"
+
+# put content of temp file of new context in the file with the old context, because after the check it is considered as old.
+cat temp.txt > $2
+
+# remove temp file
+rm temp.txt
+
+# other alternatives which did not precicly work 
 # diff -rq  $1 $2 -x '$2.zip' | grep $2 | grep -E "^Only in $2" | sed -n 's/://p' | awk '{print $3"/"$4}'
+# echo "${OUTPUT}" | grep '^deleting.*zip' | grep ' .*zip' | awk '{print $2}'
+#comm -1 temp.txt $2 | grep ".*zip"
+
+
+
+
 
 # find only modified files 
 # echo "\nmodified:"
 # diff -rq $1 $2 -x '$2.zip' | grep $1 | grep -E "^Files $1" | awk '{print $2}'
+
